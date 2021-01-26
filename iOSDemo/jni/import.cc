b6781ed58808114f24ab181b4f7eda98ff4b0287
邓江洲 <707703386@qq.com>
2021-01-26 18:33:12 +0800

    start

---
+++ iOSDemo/jni/import.cc
@@ -0,0 +1,17 @@
+/**
+ * created on : 2012-07-19
+ * author : yanguoyue 
+ */
+
+void ImportLibs() {
+	
+	extern void ExportSTN();
+	ExportSTN();
+
+	extern void ExportBaseEvent();
+	ExportBaseEvent();
+
+	extern void ExportSDT();
+	ExportSDT();
+
+}
---
+++ iOSDemo/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "e00869d6"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2021-01-26 16:44:47"
+#define MARS_TAG ""
+
+#endif
